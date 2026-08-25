9e2bf79b5b2a374a057f2a0cd955a44324d907dc
Kaivalya Thombare <itskv5107.kt@gmail.com>
2026-08-25 18:42:35 +0530

    Delete backend directory

--- backend/app/ml/__init__.py
+++
@@ -1,7 +0,0 @@
-"""ML and Tamper Detection Module"""
-
-from .predictor import DocumentPredictor
-from .tamper_detector import TamperDetector
-from .features import FeatureExtractor
-
-__all__ = ['DocumentPredictor', 'TamperDetector', 'FeatureExtractor']
